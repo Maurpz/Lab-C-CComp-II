d3979f5798d806495019c6b1773f53479575d85d
maurpz <jhualpal@unsa.edu.pe>
2022-04-22 15:48:18 -0500

    ejercicio 1

---
+++ lab_1/LAB01_GRUPO_C_EJECUTABLE_20213123_JOSE_HUALPA.cpp
@@ -0,0 +1,10 @@
+/*1*/
+#include <iostream>
+using namespace std;
+int main(){
+    int a=0;int b =0;
+    cout<<"Ingrese un numero entero : ";cin>>a;
+    cout<<"Ingrese otro numero entero : ";cin>>b;
+    cout<<"El producto de "<<a<<" x "<<b<<" es igual a : "<<a*b<<endl;
+    return 0;
+}
