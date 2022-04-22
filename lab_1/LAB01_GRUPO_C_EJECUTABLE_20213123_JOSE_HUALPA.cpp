42e9cef057fb6cff2d3bee69d9aa3658b5017055
maurpz <jhualpal@unsa.edu.pe>
2022-04-22 16:03:28 -0500

    ejercicio 2

--- lab_1/LAB01_GRUPO_C_EJECUTABLE_20213123_JOSE_HUALPA.cpp
+++ lab_1/LAB01_GRUPO_C_EJECUTABLE_20213123_JOSE_HUALPA.cpp
@@ -6,5 +6,11 @@ int main(){
     cout<<"Ingrese un numero entero : ";cin>>a;
     cout<<"Ingrese otro numero entero : ";cin>>b;
     cout<<"El producto de "<<a<<" x "<<b<<" es igual a : "<<a*b<<endl;
+
+    string nombre,apellido_p,apellido_m;
+    cout<<"Ingrese su primer nombre : ";cin>>nombre;
+    cout<<"Ingrese su apellido paterno : ";cin>>apellido_p;
+    cout<<"Ingrese su apellido materno : ";cin>>apellido_m;
+    cout<<"Su correo institucional es : "<<nombre[0]<<apellido_p<<apellido_m[0]<<"@unsa.edu.pe"<<endl;
     return 0;
 }
